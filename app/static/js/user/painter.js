5c1462bad50c237a5d78d29f3fb13c678751f975
Chyld Medford <chyld.medford@gmail.com>
2014-08-15 15:03:34 -0500

    done

--- app/static/js/user/painter.js
+++ app/static/js/user/painter.js
@@ -3,7 +3,28 @@
 
   $(document).ready(function(){
     $('#controls > button').click(addColor);
+    $('#colors').on('click', '.color', selectColor);
+    $('#canvas').on('mouseenter', '.pixel', paintPixel);
+    makeCanvas();
   });
+
+  function paintPixel(){
+    var color = $('#selected').css('background-color');
+    $(this).css('background-color', color);
+  }
+
+  function makeCanvas(){
+    for(var i = 0; i < 10000; i++){
+      var $pixel = $('<div>');
+      $pixel.addClass('pixel');
+      $('#canvas').append($pixel);
+    }
+  }
+
+  function selectColor(){
+    var color = $(this).css('background-color');
+    $('#selected').css('background-color', color);
+  }
 
   function addColor(){
     var color  = $('#controls > input').val(),
